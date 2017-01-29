c75800ef6fca7852226ad47a7a835dbd7f703b84
Shalvah Adebayo <shalvah@users.noreply.github.com>
2017-01-29 10:31:41 +0100

    Update UnnPortalException.php

--- src/UnnPortalException.php
+++ src/UnnPortalException.php
@@ -4,5 +4,8 @@ namespace UnnPortal;
 
 class UnnPortalException extends Exception 
 {
-
+    public function getMessage()
+    {
+      return "Authentication failed: Invalid username or password";
+    }
 }
